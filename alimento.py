106fbac6d5908f6ab05c2e885b163dd343ecee65
gustavoloyola <gustavoloyola287@gmail.com>
2024-06-13 03:42:15 -0300

    otro

--- alimento.py
+++ alimento.py
@@ -1,1 +1,24 @@
+#Clase hija Alimento
 
+from producto import Producto
+
+class Alimento(Producto):
+    
+    def __init__(self, nombre, precio, cantidad, fecha_expiracion):
+        
+        super().__init__(nombre, precio, cantidad)
+        self.fecha_expiracion = fecha_expiracion
+        
+        
+    def get_fecha_expiracion(self):
+        return self.fecha_expiracion
+    
+    def mostrar_informacion(self):
+        super().mostrar_informacion()
+        print(self.get_fecha_expiracion())
+
+fecha_expiracion = input("Ingrese fecha de expiracion:  : ") 
+
+mi_alimento = Alimento(nombre, fecha_expiracion)
+
+mi_alimento.mostrar_informacion()
