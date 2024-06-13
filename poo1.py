0aff7e8ef8d4618ab801c6534753be8aa4347c97
gustavoloyola <gustavoloyola287@gmail.com>
2024-06-12 22:52:20 -0300

    segundo commit

--- poo1.py
+++ poo1.py
@@ -13,13 +13,15 @@ class Producto():
     def get_cantidad(self):
         return self.cantidad
     
-    
-    
-    
-    
+    def mostrar_producto(self):
+        print("El producto se llama: ",self.get_nombre(),"y cuesta: ",self.get_precio(),"pesos")
     
 nombre = input("Ingrese nombre del producto: ")
 precio = input("Ingrese precio del producto: ")
 cantidad = input("Ingrese cantidad del producto: ")
 
+mi_producto = Producto(nombre, precio, cantidad)
+mi_producto.mostrar_producto()
+
+#print(mi_producto.mostrar_producto())
     
